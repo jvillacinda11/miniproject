b5ae337f232aeb1fed76d458544dea59c77466b7
Nick Palmer <ylnickpalmer@gmail.com>
2021-03-10 13:48:46 -0800

    created get and post methods for tables

--- controllers/tableControllers.js
+++ controllers/tableControllers.js
@@ -1,11 +1,23 @@
 const router = require('express').Router()
+const tables = require('../db')
 
-router.get('/blogs', (req, res) => {
-
+router.get('/tables', (req, res) => {
+  res.json({
+    status: 200,
+    message: "Way to go!",
+    data: tables
+  })
 })
 
-router.get('/one_blog', (req, res) => {
+router.post('/one_table', (req, res) => {
+  let newTable = {
+    name: req.body.name,
+    date: req.body.date,
+  }
 
+  tables.push(newTable)
+
+  res.sendStatus(200)
 })
 
 
---
+++ db/index.js
@@ -0,0 +1,8 @@
+module.exports = [
+  {
+    name: "Example",
+    id: 1234,
+    email: "example@example.net",
+    phone: 1234567890
+  }
+]
